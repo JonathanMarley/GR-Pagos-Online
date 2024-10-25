8f238c5051d8465c8952d587911366e1d6ae6aef
Jonathan Ignacio Marley Ramirez <jonathanmarley2536@gmail.com>
2024-10-25 15:19:18 -0600

    segundor commit

--- src/pages/BienesInmuebles.tsx
+++ src/pages/BienesInmuebles.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 import Sidebar from '../Components/Sidebar';
 import Header from '../Components/Header';
 import DetallesBienesInmubles from '../Components/BienesInmueblesDetalles'
